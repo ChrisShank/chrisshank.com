2625fb57520fe949791abbdc8d4aa62f3532618f
chrisshank <chris.shank.23@gmail.com>
2019-07-11 18:54:41 -0400

    Started designing site. Created FlipCard component.

---
+++ src/components/FlipCard.svelte
@@ -0,0 +1,53 @@
+<script>
+  export let height = 200;
+  export let width = 300;
+  export let dimensionUnit = "px";
+</script>
+
+<style>
+  .flip-card {
+    perspective: 1000px;
+  }
+
+  .flip-card-inner {
+    height: 100%;
+    position: relative;
+    text-align: center;
+    transition: transform 0.8s ease-out;
+    transform-style: preserve-3d;
+    width: 100%;
+    will-change: transform;
+  }
+
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+
+  .flip-card-front,
+  .flip-card-back {
+    backface-visibility: hidden;
+    background-color: rgb(245, 239, 239);
+    border-radius: 7px;
+    box-shadow: 0 6px 20px rgba(36, 37, 38, 0.75);
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+
+  .flip-card-back {
+    transform: rotateY(180deg);
+  }
+</style>
+
+<div
+  class="flip-card"
+  style="height: {height + dimensionUnit}; width: {width + dimensionUnit};">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <slot name="front" />
+    </div>
+    <div class="flip-card-back">
+      <slot name="back" />
+    </div>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,20 @@
-<h1>Heading 1</h1>
-<h2>Heading 2</h2>
-<h3>Heading 3</h3>
-<p>Paragraph1</p>
-<p>
-  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-  veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-  ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-  consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-  porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-  adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-  dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-  nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-  ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
-  voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem
-  eum fugiat quo voluptas nulla pariatur?"
-</p>
+<script>
+  import FlipCard from "../components/FlipCard.svelte";
+</script>
+
+<style>
+  .container {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    min-width: 320px;
+  }
+</style>
+
+<div class="container">
+  <FlipCard height={11.089} width={17.942} dimensionUnit="rem">
+    <h3 slot="front" class="card-front">Chris Shank</h3>
+    <h3 slot="back" class="card-back">Hello World</h3>
+  </FlipCard>
+</div>
