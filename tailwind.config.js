8de5036fb2a70bde603229704414cf6432f89eb8
chrisshank <chris.shank.23@gmail.com>
2020-05-26 23:29:17 -0400

    refactor responsive layout, add footer, add font weight utilities for variant font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,41 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
 	purge: ['./index.html'],
+	corePlugins: {
+		fontWeight: false,
+	},
+	plugins: [
+		plugin(({ addUtilities }) => {
+			addUtilities({
+				'.font-hairline': {
+					'font-variation-settings': '\'wght\' 100',
+				},
+				'.font-thin': {
+					'font-variation-settings': '\'wght\' 200',
+				},
+				'.font-light': {
+					'font-variation-settings': '\'wght\' 300',
+				},
+				'.font-normal': {
+					'font-variation-settings': '\'wght\' 400',
+				},
+				'.font-medium': {
+					'font-variation-settings': '\'wght\' 500',
+				},
+				'.font-semibold': {
+					'font-variation-settings': '\'wght\' 600',
+				},
+				'.font-bold': {
+					'font-variation-settings': '\'wght\' 700',
+				},
+				'.font-extrabold': {
+					'font-variation-settings': '\'wght\' 800',
+				},
+				'.font-black': {
+					'font-variation-settings': '\'wght\' 900',
+				},
+			})
+		})
+	],
 }
