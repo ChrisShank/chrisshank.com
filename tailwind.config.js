ab162e150846a291db80d9d181b220399761a5d8
chrisshank <chris.shank.23@gmail.com>
2020-05-26 23:36:20 -0400

    refactored font weight plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,35 +7,22 @@ module.exports = {
 	},
 	plugins: [
 		plugin(({ addUtilities }) => {
-			addUtilities({
-				'.font-hairline': {
-					'font-variation-settings': '\'wght\' 100',
-				},
-				'.font-thin': {
-					'font-variation-settings': '\'wght\' 200',
-				},
-				'.font-light': {
-					'font-variation-settings': '\'wght\' 300',
-				},
-				'.font-normal': {
-					'font-variation-settings': '\'wght\' 400',
-				},
-				'.font-medium': {
-					'font-variation-settings': '\'wght\' 500',
-				},
-				'.font-semibold': {
-					'font-variation-settings': '\'wght\' 600',
-				},
-				'.font-bold': {
-					'font-variation-settings': '\'wght\' 700',
-				},
-				'.font-extrabold': {
-					'font-variation-settings': '\'wght\' 800',
-				},
-				'.font-black': {
-					'font-variation-settings': '\'wght\' 900',
-				},
-			})
+			const fontWeights = [
+				'.font-hairline',
+				'.font-thin',
+				'.font-light',
+				'.font-normal',
+				'.font-medium',
+				'.font-semibold',
+				'.font-bold',
+				'.font-extrabold',
+				'.font-black',
+			].reduce((acc, className, i) => {
+				acc[className] = { 'font-variation-settings': `'wght' ${(i + 1) * 100}` }
+				return acc
+			}, {})
+
+			addUtilities(fontWeights)
 		})
 	],
 }
