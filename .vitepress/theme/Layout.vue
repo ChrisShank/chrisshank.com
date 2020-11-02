21ef52db19d23ecabc293916329ea5c8fd675441
chrisshank <chris.shank.23@gmail.com>
2020-11-01 21:34:51 -0800

    feat(digital-garden): init digital garden design

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="min-h-screen flex flex-col p-4 bg-gray-200">
+		<header class="mb-4">
+			<h1>
+				<a class="italic font-semibold text-lg" href="/"> Chris Shank </a>
+			</h1>
+		</header>
+
+		<main class="prose flex-1 mx-auto">
+			<Content />
+		</main>
+
+		<hr class="border-gray-400" />
+
+		<footer class="text-right">
+			©<span class="font-light">ChrisShank</span><span class="font-extrabold">2020</span>
+		</footer>
+	</div>
+</template>
---
+++ .vitepress/theme/index.js
@@ -0,0 +1,11 @@
+import Layout from './Layout.vue'
+import NotFound from './NotFound.vue'
+
+import './main.css'
+
+export default {
+	Layout,
+	NotFound,
+	enhanceApp({ app, router, siteData }) {
+	}
+}
